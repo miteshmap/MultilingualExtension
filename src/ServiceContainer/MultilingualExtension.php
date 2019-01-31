84786f96fd848fd5bd7f9d83c9846ba4a21a7cbf
Mitesh Patel <developermitesh@gmail.com>
2019-01-31 19:44:01 +0530

    Override gherkin's service of filediscovery.

--- src/ServiceContainer/MultilingualExtension.php
+++ src/ServiceContainer/MultilingualExtension.php
@@ -4,11 +4,15 @@
  */
 namespace kolevCustomized\MultilingualExtension\ServiceContainer;
 
+use Behat\Behat\Gherkin\ServiceContainer\GherkinExtension;
 use Behat\EnvironmentLoader;
 use Behat\Testwork\ServiceContainer\Extension;
 use Behat\Testwork\ServiceContainer\ExtensionManager;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Behat\Testwork\Specification\ServiceContainer\SpecificationExtension;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\Definition;
 
 /**
  * Class MultilingualExtension.
@@ -39,6 +43,7 @@ class MultilingualExtension implements Extension
     {
         $loader = new EnvironmentLoader($this, $container, $config);
         $loader->load();
+        $this->loadDecoratedFilesystemFeatureLocator($container);
     }
 
     /**
@@ -57,5 +62,29 @@ class MultilingualExtension implements Extension
         $config->scalarNode('default_language')->end();
         $config->arrayNode('translations')->prototype('scalar')->end();
         $config->end();
+    }
+
+    /**
+     * Loads filesystem feature locator.
+     *
+     * @param ContainerBuilder $container
+     */
+    /*private function loadFilesystemFeatureLocator(ContainerBuilder $container) {
+      $definition = new Definition('kolevCustomized\MultilingualExtension\Specification\Locator\FilesystemFeatureLocator', array(
+        new Reference(GherkinExtension::MANAGER_ID),
+        '%paths.base%'
+      ));
+      $definition->addTag(SpecificationExtension::LOCATOR_TAG, array('priority' => 600));
+      $container->setDefinition(SpecificationExtension::LOCATOR_TAG . '.alshaya_custom_feature_iterator', $definition);
+    }*/
+
+    private function loadDecoratedFilesystemFeatureLocator(ContainerBuilder $container) {
+      $definition = new Definition('kolevCustomized\MultilingualExtension\Specification\Locator\FilesystemFeatureLocator', array(
+        new Reference(GherkinExtension::MANAGER_ID),
+        '%paths.base%'
+      ));
+      $definition->addTag(SpecificationExtension::LOCATOR_TAG, array('priority' => 600));
+      $definition->setDecoratedService(SpecificationExtension::LOCATOR_TAG . '.filesystem_feature', SpecificationExtension::LOCATOR_TAG . '.filesystem_feature.inner', 9);
+      $container->setDefinition(SpecificationExtension::LOCATOR_TAG . '.alshaya_custom_feature_iterator', $definition);
     }
 }
